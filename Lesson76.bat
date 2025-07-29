5112ceb78fcae8bf3dd481ecdeaa75d39781eb9a
highe <highe@192.168.151.42>
2025-07-29 12:57:03 +0900

    練習問題76をコミットします。

---
+++ Lesson76.bat
@@ -0,0 +1,7 @@
+
+javac -encoding UTF-8 -d bin src\gitproject\Lesson76.java
+
+
+java -cp bin gitproject.Lesson76
+
+pause
---
+++ src/gitproject/Lesson76.java
@@ -0,0 +1,10 @@
+package gitproject;
+
+public class Lesson76 {
+
+	public static void main(String[] args) {
+		System.out.println("Lesson76のバッチファイル作成問題のJavaソースです。");
+
+	}
+
+}
